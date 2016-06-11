298b8d258d5cda7b459c86411c57e8947cbc5778
Thomas Gaubert <tpgaubert@gmail.com>
2016-06-10 21:33:42 -0700

    [Desktop] Update menu and link colors

--- desktop/app/res/js/menu.js
+++ desktop/app/res/js/menu.js
@@ -4,44 +4,6 @@ function setupMenu() {
 
 	const template = [
 	  {
-	    label: 'Edit',
-	    submenu: [
-	      {
-	        label: 'Undo',
-	        accelerator: 'CmdOrCtrl+Z',
-	        role: 'undo'
-	      },
-	      {
-	        label: 'Redo',
-	        accelerator: 'Shift+CmdOrCtrl+Z',
-	        role: 'redo'
-	      },
-	      {
-	        type: 'separator'
-	      },
-	      {
-	        label: 'Cut',
-	        accelerator: 'CmdOrCtrl+X',
-	        role: 'cut'
-	      },
-	      {
-	        label: 'Copy',
-	        accelerator: 'CmdOrCtrl+C',
-	        role: 'copy'
-	      },
-	      {
-	        label: 'Paste',
-	        accelerator: 'CmdOrCtrl+V',
-	        role: 'paste'
-	      },
-	      {
-	        label: 'Select All',
-	        accelerator: 'CmdOrCtrl+A',
-	        role: 'selectall'
-	      },
-	    ]
-	  },
-	  {
 	    label: 'View',
 	    submenu: [
 	      {
