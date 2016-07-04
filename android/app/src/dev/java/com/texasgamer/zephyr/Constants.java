ced74e575d642b23ccbd232faafa103e4bce55a3
Thomas Gaubert <tpgaubert@gmail.com>
2016-07-03 17:05:33 -0700

    [Android] Disable metrics during development

---
+++ android/app/src/dev/java/com/texasgamer/zephyr/Constants.java
@@ -0,0 +1,11 @@
+package com.texasgamer.zephyr;
+
+public class Constants {
+
+    public static final String FIREBASE_ADDRESS = "https://zephyr-99819.firebaseio.com/";
+    public static final boolean FIREBASE_ANALYTICS_ENABLED = false;
+    public static final boolean FIREBASE_LOGIN_ENABLED = true;
+
+    public static final boolean FABRIC_CRASHLYITCS_ENABLED = false;
+    public static final boolean FABRIC_ANSWERS_ENABLED = false;
+}
