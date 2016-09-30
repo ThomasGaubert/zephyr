a980f0c025d8350b9046d0d1b474f2f0f9bcec69
Thomas Gaubert <tpgaubert@gmail.com>
2016-09-30 00:55:19 -0500

    [Desktop] Enable CORS support

--- desktop/app/main.js
+++ desktop/app/main.js
@@ -28,6 +28,13 @@ const uuid = conf.get('uuid')
 // Networking
 var bodyParser = require('body-parser')
 var web = require('express')()
+web.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "X-Requested-With")
+  res.header("Access-Control-Allow-Headers", "Content-Type")
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS")
+  next()
+})
 web.use(bodyParser.urlencoded({ extended: false }))
 web.use(bodyParser.json())
 var http = require('http').Server(web)
