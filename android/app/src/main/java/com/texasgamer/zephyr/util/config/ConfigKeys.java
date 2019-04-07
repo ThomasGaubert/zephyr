d7c65c742e5f6912543abfd2b5d1cefe03a1c43f
Thomas Gaubert <tpgaubert@gmail.com>
2019-04-06 18:45:38 -0700

    Add ripple to join code button

--- android/app/src/main/java/com/texasgamer/zephyr/util/config/ConfigKeys.java
+++ android/app/src/main/java/com/texasgamer/zephyr/util/config/ConfigKeys.java
@@ -3,8 +3,8 @@ package com.texasgamer.zephyr.util.config;
 /**
  * Configuration keys.
  */
-public final class ConfigKeys {
-    public static final String ENABLE_SCAN_QR_CODE = "enableScanQrCode";
+final class ConfigKeys {
+    static final String ENABLE_SCAN_QR_CODE = "enableScanQrCode";
 
     private ConfigKeys() {
     }
--- android/app/src/main/java/com/texasgamer/zephyr/util/preference/PreferenceKeys.java
+++ android/app/src/main/java/com/texasgamer/zephyr/util/preference/PreferenceKeys.java
@@ -8,6 +8,8 @@ public final class PreferenceKeys {
     public static final String PREF_FRE = "completedFre";
     public static final String PREF_LAST_KNOWN_APP_VERSION = "lastKnownAppVersion";
     public static final String PREF_IS_CONNECTED = "isConnected";
+    public static final String PREF_IS_SOCKET_SERVICE_RUNNING = "isSocketServiceRunning";
+    public static final String PREF_CONNECTION_STATUS = "connectionStatus";
     public static final String PREF_JOIN_CODE = "joinCode";
 
     private PreferenceKeys() {
