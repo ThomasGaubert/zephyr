1ba645999499040e28e4260b1b1036f2d2aba5f6
Thomas Gaubert <tpgaubert@gmail.com>
2019-12-03 22:46:41 -0800

    More checkstyle, set node version

--- android/app/src/main/java/com/texasgamer/zephyr/util/eventbus/EventBusEvent.java
+++ android/app/src/main/java/com/texasgamer/zephyr/util/eventbus/EventBusEvent.java
@@ -5,7 +5,7 @@ import com.texasgamer.zephyr.BuildConfig;
 /**
  * EventBus events.
  */
-public class EventBusEvent {
+public final class EventBusEvent {
     public static final String SHELL_REFRESH_CARDS = BuildConfig.APPLICATION_ID + ".shell.REFRESH_CARDS";
 
     private EventBusEvent() {
